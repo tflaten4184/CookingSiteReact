684f4968a3c262b4a913ce962ce0a6920159c5d7
tflaten4184 <tflaten4184@gmail.com>
2023-05-30 14:14:38 -0700

    basic navbar and styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import { Container } from 'react-bootstrap';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import RecipeScreen from './screens/RecipeScreen';
 import PageNotFound from './screens/PageNotFound';
 
 import './App.css';
+import AboutScreen from './screens/AboutScreen';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Navbar />
       <Container>
         <Routes>
           <Route path='/' element={<HomeScreen />}></Route>
           <Route path='/recipe/:id' element={<RecipeScreen />}></Route>
+          <Route path='/about/' element={<AboutScreen />}></Route>
           <Route path='*' element={<PageNotFound />}></Route>
         </Routes>
       </Container>
---
+++ src/screens/AboutScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AboutScreen() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is my portfolio project, made with React and Django. I made this for sharing my favorite recipes, along with a cost-per-ingredient cost-per-recipe.</p>
+    </div>
+  )
+}
+
+export default AboutScreen
