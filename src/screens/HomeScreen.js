5e5adf7ab238ef607007a2644a05b316ea40577b
tflaten4184 <tflaten4184@gmail.com>
2023-05-29 18:22:03 -0700

    axios API for fetching recipes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
+import axios from "axios";
 
 import Recipe from "../components/Recipe";
 
 import recipes from "../recipes";
 
 function HomeScreen() {
-//   const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    async function fetchRecipes() {
+      console.log("before axios");
+      const { data } = await axios.get("http://127.0.0.1:8000/api/recipes/");
+      console.log(data);
+      setRecipes(data);
+    }
+
+    fetchRecipes();
+  }, []);
+
   return (
     <div>
       HomeScreen
-
       <Row>
         {recipes.map((recipe) => (
           <Col key={recipe.id}>
