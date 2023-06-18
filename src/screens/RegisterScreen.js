1e8440b397bb3f3b5ea0350972b248a9edf2a397
tflaten4184 <tflaten4184@gmail.com>
2023-06-18 11:06:31 -0700

    API request, create account with form

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -7,24 +7,56 @@ function RegisterScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const submit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:8000/api/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
+
+    const content = await response.json();
+
+    console.log(content);
+  };
 
   return (
-    <Form>
+    <Form onSubmit={submit}>
       <h1>Register</h1>
 
       <Form.Group className="mb-3" controlId="formBasicUsername">
         <Form.Label>Username</Form.Label>
-        <Form.Control type="username" placeholder="Enter username" required />
+        <Form.Control
+          type="username"
+          placeholder="Enter username"
+          required
+          onChange={(e) => setName(e.target.value)}
+        />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" required />
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" required />
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
