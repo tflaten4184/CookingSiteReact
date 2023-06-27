f48f95af2d45ef93d555c7646e9ef67862bde2c6
tflaten4184 <tflaten4184@gmail.com>
2023-06-27 10:22:06 -0700

    API request login JWT cookie

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,24 +1,50 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useState } from "react";
+import { Navigate } from 'react-router-dom'
+
+
 
 function LoginScreen() {
-  return (
-    <Form>
-      <h1>Login</h1>
 
-      <Form.Group className="mb-3" controlId="formBasicUsername">
-        <Form.Label>Username</Form.Label>
-        <Form.Control type="username" placeholder="Enter username" required />
-      </Form.Group>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+
+  const submit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:8000/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: 'include',
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    const content = await response.json();
+    console.log(content);
+    setRedirect(true);
+  }
+
+  if (redirect) {
+    return <Navigate to="/" />
+  } 
+
+  return (
+    <Form onSubmit={submit}>
+      <h1>Login</h1>
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" required />
+        <Form.Control type="email" placeholder="Enter email" required onChange={(e) => setEmail(e.target.value)} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" required />
+        <Form.Control type="password" placeholder="Password" required onChange={(e) => setPassword(e.target.value)} />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
